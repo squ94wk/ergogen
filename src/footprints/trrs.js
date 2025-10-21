a0401ae26f4b08bd2da78c2cf76323b6d8100972
Josef Mende <squ94wk@gmail.com>
2025-10-22 00:18:40 +0200

    Position board on pcb

--- src/footprints/trrs.js
+++ src/footprints/trrs.js
@@ -1,103 +1,160 @@
-// TRRS-PJ-320A-dual
+// Copyright (c) 2023 Marco Massarelli
 //
-// Normal footprint:
-//     _________________
-//    |   (2)   (3) (4)|
-//    |                |
-//    | (1)            |
-//    |________________|
-// 
-// Reverse footprint:
-//     _________________
-//    |   (2)   (3) (4)|
-//    | (1)            |
-//    | (1)            |
-//    |___(2)___(3)_(4)|
+// SPDX-License-Identifier: MIT
 //
-// Reverse & symmetric footprint:
-//     _________________
-//    | (1|2)   (3) (4)|
-//    |                |
-//    |_(1|2)___(3)_(4)|
+// To view a copy of this license, visit https://opensource.org/license/mit/
 //
-// Nets
-//    A: corresponds to pin 1
-//    B: corresponds to pin 2
-//    C: corresponds to pin 3
-//    D: corresponds to pin 4
-// Params
-//    reverse: default is false
-//      if true, will flip the footprint such that the pcb can be reversible
+// Authors: @ergogen + @ceoloide improvements
+//
+// Description:
+//  A reversible "PJ-320A" TRRS footprint similar to the one used on
+//  the Corne keyboard, and available at LCSC. The footprint offers many
+//  customization options.
+//
+//  Normal / single side
+//     ____________________
+//    |   (TP)   (R2) (SL)|_
+//    |                   | |
+//    | (R1)              |_|
+//    |___________________|
+//
+//  Reversible
+//     ____________________
+//    |   (TP)   (R2) (SL)|_
+//    | (R1)              | |
+//    | (R1)              |_|
+//    |___(TP)___(R2)_(SL)|
+//
+// Reversible & symmetrical
+//     ___________________
+//    | ( TP) (R2)   (SL)|_
+//    |                  |_|
+//    |_( TP)_(R2)___(SL)|
+//
+// Datasheet:
+//  https://datasheet.lcsc.com/lcsc/2311241628_Hong-Cheng-HC-PJ-320A_C7501806.pdf
+//
+// Nets:
+//    SL: corresponds to pin 2 (Sleeve)
+//    R2: corresponds to pin 3 (Ring 2)
+//    R1: corresponds to pin 1 (Ring 1)
+//    TP: corresponds to pin 4 (Tip)
+//
+// Warning:
+//    TRRS cables should never be hotswapped (removed or inserted when the MCU is turned on).
+//    To minimize the chance of damaging the MCU, connect VCC to the tip (TP) and GND on the
+//    sleeve (SL).
+//
+// Params:
+//    side: default is F for Front
+//      the side on which to place the single-side footprint and designator, either F or B
+//    reversible: default is false
+//      if true, the footprint will be placed on both sides so that the PCB can be
+//      reversible
 //    symmetric: default is false
-//      if true, will only work if reverse is also true
-//      this will cause the footprint to be symmetrical on each half
-//      pins 1 and 2 must be identical if symmetric is true, as they will overlap
+//      if true, it will only work if reversible is also true. This will cause the
+//      footprint to be symmetrical on each half, however reducing the footprint
+//      to three pins: TP, R2, and SL
+//
+// @ceoloide's improvements:
+//  - Add oval pad when symmetrical
+//  - Adjust positioning to be symmetrical
+//  - Revamp pinout and nets
+//  - Upgrade to KiCad 8
 
 module.exports = {
-  params: {
-    designator: 'TRRS',
-    reverse: false,
-    symmetric: false,
-    A: undefined,
-    B: undefined,
-    C: undefined,
-    D: undefined
-  },
-  body: p => {
-    const standard = `
-      (module TRRS-PJ-320A-dual (layer F.Cu) (tedit 5970F8E5)
+    params: {
+        designator: 'TRRS',
+        side: 'F',
+        reversible: false,
+        symmetric: false,
+        TP: { type: 'net', value: 'TP' },
+        R1: { type: 'net', value: 'R1' },
+        R2: { type: 'net', value: 'R2' },
+        SL: { type: 'net', value: 'SL' },
+    },
+    body: p => {
 
-      ${p.at /* parametric position */}   
+        let footprint_name = "trrs_pj320a"
+        if (p.reversible) {
+            if (p.symmetric) {
+                footprint_name += " (reversible, symmetric)"
+            } else {
+                footprint_name += " (reversible)"
+            }
+        }
 
-      ${'' /* footprint reference */}
-      (fp_text reference "${p.ref}" (at 0 14.2) (layer Dwgs.User) (effects (font (size 1 1) (thickness 0.15))))
-      (fp_text value TRRS-PJ-320A-dual (at 0 -5.6) (layer F.Fab) (effects (font (size 1 1) (thickness 0.15))))
-
-      ${''/* corner marks */}
-      (fp_line (start 0.5 -2) (end -5.1 -2) (layer Dwgs.User) (width 0.15))
-      (fp_line (start -5.1 0) (end -5.1 -2) (layer Dwgs.User) (width 0.15))
-      (fp_line (start 0.5 0) (end 0.5 -2) (layer Dwgs.User) (width 0.15))
-      (fp_line (start -5.35 0) (end -5.35 12.1) (layer Dwgs.User) (width 0.15))
-      (fp_line (start 0.75 0) (end 0.75 12.1) (layer Dwgs.User) (width 0.15))
-      (fp_line (start 0.75 12.1) (end -5.35 12.1) (layer Dwgs.User) (width 0.15))
-      (fp_line (start 0.75 0) (end -5.35 0) (layer Dwgs.User) (width 0.15))
-
+        const standard_opening = `
+  (footprint "ceoloide:${footprint_name}"
+    (layer "${p.side}.Cu")
+    ${p.at}
+    (property "Reference" "${p.ref}"
+      (at 0 14.2 ${p.r})
+      (layer "${p.side}.SilkS")
+      ${p.ref_hide}
+      (effects (font (size 1 1) (thickness 0.15)))
+    )
+    `
+        function corner_marks(offset_x) {
+            return `
+    (fp_line (start ${2.8 + offset_x} -2) (end ${-2.8 + offset_x} -2) (layer "Dwgs.User") (stroke (width 0.15) (type solid)))
+    (fp_line (start ${-2.8 + offset_x} 0) (end ${-2.8 + offset_x} -2) (layer "Dwgs.User") (stroke (width 0.15) (type solid)))
+    (fp_line (start ${2.8 + offset_x} 0) (end ${2.8 + offset_x} -2) (layer "Dwgs.User") (stroke (width 0.15) (type solid)))
+    (fp_line (start ${-3.05 + offset_x} 0) (end ${-3.05 + offset_x} 12.1) (layer "Dwgs.User") (stroke (width 0.15) (type solid)))
+    (fp_line (start ${3.05 + offset_x} 0) (end ${3.05 + offset_x} 12.1) (layer "Dwgs.User") (stroke (width 0.15) (type solid)))
+    (fp_line (start ${3.05 + offset_x} 12.1) (end ${-3.05 + offset_x} 12.1) (layer "Dwgs.User") (stroke (width 0.15) (type solid)))
+    (fp_line (start ${3.05 + offset_x} 0) (end ${-3.05 + offset_x} 0) (layer "Dwgs.User") (stroke (width 0.15) (type solid)))
       `
-    function stabilizers(def_pos) {
-      return `
-        (pad "" np_thru_hole circle (at ${def_pos} 8.6) (size 1.5 1.5) (drill 1.5) (layers *.Cu *.Mask))
-        (pad "" np_thru_hole circle (at ${def_pos} 1.6) (size 1.5 1.5) (drill 1.5) (layers *.Cu *.Mask))
+        }
+        function stabilizers(def_pos) {
+            return `
+    (pad "" np_thru_hole circle (at ${def_pos} 8.6 ${p.r}) (size 1.5 1.5) (drill 1.5) (layers "*.Cu" "*.Mask"))
+    (pad "" np_thru_hole circle (at ${def_pos} 1.6 ${p.r}) (size 1.5 1.5) (drill 1.5) (layers "*.Cu" "*.Mask"))
       `
-    }
-    function pins(def_neg, def_pos) {
-      return `
-        (pad 1 thru_hole oval (at ${def_neg} 11.3 ${p.r}) (size 1.6 2.2) (drill oval 0.9 1.5) (layers *.Cu *.Mask) ${p.A})
-        (pad 2 thru_hole oval (at ${def_pos} 10.2 ${p.r}) (size 1.6 2.2) (drill oval 0.9 1.5) (layers *.Cu *.Mask) ${p.B})
-        (pad 3 thru_hole oval (at ${def_pos} 6.2 ${p.r}) (size 1.6 2.2) (drill oval 0.9 1.5) (layers *.Cu *.Mask) ${p.C})
-        (pad 4 thru_hole oval (at ${def_pos} 3.2 ${p.r}) (size 1.6 2.2) (drill oval 0.9 1.5) (layers *.Cu *.Mask) ${p.D})
-      `
-    }
-    if(p.reverse & p.symmetric) {
-      return `
-        ${standard}
-        ${stabilizers('-2.3')}
-        ${pins('0', '-4.6')}
-        ${pins('-4.6', '0')})
-      `
-    } else if(p.reverse) {
-        return `
-          ${standard}
-          ${stabilizers('-2.3')}
-          ${stabilizers('0')}
-          ${pins('-2.3', '2.3')}
-          ${pins('0', '-4.6')})
+        }
+        function pins(def_neg, def_pos) {
+            if (p.symmetric && p.reversible) {
+                return `
+    (pad 2 thru_hole oval (at ${def_pos} 3.2 ${p.r}) (size 1.6 2.2) (drill oval 0.9 1.5) (layers "*.Cu" "*.Mask") ${p.SL.str})
+    (pad 3 thru_hole oval (at ${def_pos} 6.2 ${p.r}) (size 1.6 2.2) (drill oval 0.9 1.5) (layers "*.Cu" "*.Mask") ${p.R2.str})
+    (pad 4 thru_hole oval (at ${def_pos} 10.75 ${p.r}) (size 1.6 3.3) (drill oval 0.9 2.6) (layers "*.Cu" "*.Mask") ${p.TP.str})
         `
-    } else {
-      return `
-        ${standard}
-        ${stabilizers('-2.3')}
-        ${pins('-4.6', '0')})
+            } else {
+                return `
+    (pad 2 thru_hole oval (at ${def_pos} 3.2 ${p.r}) (size 1.6 2.2) (drill oval 0.9 1.5) (layers "*.Cu" "*.Mask") ${p.SL.str})
+    (pad 3 thru_hole oval (at ${def_pos} 6.2 ${p.r}) (size 1.6 2.2) (drill oval 0.9 1.5) (layers "*.Cu" "*.Mask") ${p.R2.str})
+    (pad 4 thru_hole oval (at ${def_pos} 10.2 ${p.r}) (size 1.6 2.2) (drill oval 0.9 1.5) (layers "*.Cu" "*.Mask") ${p.TP.str})
+    (pad 5 thru_hole oval (at ${def_neg} 11.3 ${p.r}) (size 1.6 2.2) (drill oval 0.9 1.5) (layers "*.Cu" "*.Mask") ${p.R1.str})
+        `
+            }
+        }
+        if (p.reversible & p.symmetric) {
+            return `
+    ${standard_opening}
+    ${corner_marks(0)}
+    ${stabilizers(0)}
+    ${pins(2.3, -2.3)}
+    ${pins(-2.3, 2.3)}
+  )
       `
+        } else if (p.reversible) {
+            return `
+    ${standard_opening}
+    ${corner_marks(1.15)}
+    ${stabilizers(-1.15)}
+    ${stabilizers(1.15)}
+    ${pins(-1.15, 3.45)}
+    ${pins(1.15, -3.45)}
+  )
+      `
+        } else {
+            return `
+    ${standard_opening}
+    ${corner_marks(0)}
+    ${stabilizers(0)}
+    ${p.side == 'F' ? pins(-2.3, 2.3) : pins(2.3, -2.3) }
+  )
+    `
+        }
     }
-  }
 }
